ef023f0d4328a98924436bff34be4298bd1fec1e
Artyom Sovetnikov <elringus@gmail.com>
2020-11-26 21:43:07 +0300

    Initial commit

---
+++ Assets/NaninovelSpreadsheet/Editor/SpreadsheetWindow.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using UnityEditor;
+using UnityEngine;
+
+namespace Naninovel.Spreadsheet
+{
+    public class SpreadsheetWindow : EditorWindow
+    {
+        protected string SpreadsheetPath { get => PlayerPrefs.GetString(GetPrefName()); set { PlayerPrefs.SetString(GetPrefName(), value); ValidatePaths(); } }
+        protected string ScriptFolderPath { get => PlayerPrefs.GetString(GetPrefName()); set => PlayerPrefs.SetString(GetPrefName(), value); }
+        protected string TextFolderPath { get => PlayerPrefs.GetString(GetPrefName()); set => PlayerPrefs.SetString(GetPrefName(), value); }
+        protected string LocalizationFolderPath { get => PlayerPrefs.GetString(GetPrefName()); set => PlayerPrefs.SetString(GetPrefName(), value); }
+
+        private static readonly GUIContent spreadsheetPathContent = new GUIContent("Spreadsheet", "The spreadsheet file (.xls or .xlsx).");
+        private static readonly GUIContent scriptFolderPathContent = new GUIContent("Scripts", "Folder containing naninovel script files (optional).");
+        private static readonly GUIContent textFolderPathContent = new GUIContent("Text", "Folder containing managed text files (optional).");
+        private static readonly GUIContent localizationFolderPathContent = new GUIContent("Localization", "Folder containing localization resources (optional).");
+
+        private bool pathsValid = false;
+        
+        [MenuItem("Naninovel/Tools/Spreadsheet")]
+        private static void OpenWindow ()
+        {
+            var position = new Rect(100, 100, 500, 200);
+            GetWindowWithRect<SpreadsheetWindow>(position, true, "Spreadsheet", true);
+        }
+        
+        private static string GetPrefName ([CallerMemberName] string name = "") => $"Naninovel.{nameof(SpreadsheetWindow)}.{name}";
+
+        private void ValidatePaths ()
+        {
+            pathsValid = File.Exists(SpreadsheetPath) && Path.GetExtension(SpreadsheetPath) == ".xls" || Path.GetExtension(SpreadsheetPath) == ".xlsx";
+        }
+        
+        private void OnGUI ()
+        {
+            EditorGUILayout.LabelField("Naninovel Spreadsheet", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("The tool to export/import scenario script, managed text and localization data to/from a spreadsheet.", EditorStyles.wordWrappedMiniLabel);
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                SpreadsheetPath = EditorGUILayout.TextField(spreadsheetPathContent, SpreadsheetPath);
+                if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
+                    SpreadsheetPath = EditorUtility.OpenFilePanel(spreadsheetPathContent.text, "", "xls,xlsx");
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                ScriptFolderPath = EditorGUILayout.TextField(scriptFolderPathContent, ScriptFolderPath);
+                if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
+                    ScriptFolderPath = EditorUtility.OpenFolderPanel(scriptFolderPathContent.text, "", "");
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                TextFolderPath = EditorGUILayout.TextField(textFolderPathContent, TextFolderPath);
+                if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
+                    TextFolderPath = EditorUtility.OpenFolderPanel(textFolderPathContent.text, "", "");
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                LocalizationFolderPath = EditorGUILayout.TextField(localizationFolderPathContent, LocalizationFolderPath);
+                if (GUILayout.Button("Select", EditorStyles.miniButton, GUILayout.Width(65)))
+                    LocalizationFolderPath = EditorUtility.OpenFolderPanel(localizationFolderPathContent.text, "", "");
+            }
+
+            GUILayout.FlexibleSpace();
+
+            if (pathsValid)
+            {
+                if (GUILayout.Button("Export", GUIStyles.NavigationButton))
+                    Export();
+                
+                if (GUILayout.Button("Import", GUIStyles.NavigationButton))
+                    Import();
+            }
+            else EditorGUILayout.HelpBox("Spreadsheet path is not valid; make sure it points to an existing .xls or .xlsx file.", MessageType.Error);
+
+            EditorGUILayout.Space();
+        }
+
+        private void Export ()
+        {
+            if (!EditorUtility.DisplayDialog("Export data to the spreadsheet?",
+                "Are you sure you want to export the scenario scripts, managed text and localization data to the spreadsheet?\n\nThe spreadsheet content will be overwritten, existing data could be lost. The effect of this action is permanent and can't be undone, so make sure to backup the spreadsheet file before confirming.", "Export", "Cancel")) return;
+
+            using (var document = SpreadsheetDocument.Open(SpreadsheetPath, false))
+            {
+                Debug.Log(GetCellValue(document, "Sheet1", "B2"));
+            }
+        }
+        
+        private void Import ()
+        {
+            if (!EditorUtility.DisplayDialog("Import data from the spreadsheet?",
+                "Are you sure you want to import the spreadsheet data to this project?\n\nAffected scenario scripts, managed text and localization documents will be overwritten, existing data could be lost. The effect of this action is permanent and can't be undone, so make sure to backup the project before confirming.", "Import", "Cancel")) return;
+            
+        }
+
+        private static string GetCellValue (SpreadsheetDocument document, string sheetName, string addressName)
+        {
+            var workbookPart = document.WorkbookPart;
+            var sheet = workbookPart.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name == sheetName);
+            if (sheet is null) throw new Exception($"Sheet `{sheetName}` not found in `{document}`.");
+
+            var worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+            var cell = worksheetPart.Worksheet.Descendants<Cell>().FirstOrDefault(c => c.CellReference == addressName);
+            if (cell?.DataType is null || cell.InnerText.Length == 0) return null;
+
+            switch (cell.DataType.Value)
+            {
+                case CellValues.SharedString:
+                    var stringTable = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                    return stringTable?.SharedStringTable.ElementAt(int.Parse(cell.InnerText)).InnerText;
+                case CellValues.Boolean:
+                    return cell.InnerText == "0" ? "FALSE" : "TRUE";
+                default: return null;
+            }
+        }
+    }
+}
